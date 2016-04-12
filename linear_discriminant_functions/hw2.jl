b239c714114197f66e72bfb5d1fe8aed8fbc878a
Xiang Zhang <zxteloiv@outlook.com>
2016-04-12 23:49:12 +0800

    😢  add relax procedure, but it still doesn't converge

--- linear_discriminant_functions/hw2.jl
+++ linear_discriminant_functions/hw2.jl
@@ -8,47 +8,89 @@ w41 = [2.0, 8.9, 4.2, 8.5, 6.7, 0.5, 5.3, 8.7, 7.1, 8.0] * -1;
 w42 = [8.4, -0.2, 7.7, 3.2, 4.0, 9.2, 6.7, 6.4, 9.7, 6.3] * -1;
 const SAMPLE = [w11 w12 w21 w22 w31 w32 w41 w42];
 
-" normalization adds additional one column to data "
-normalize(data) = [data ones(size(data)[1])]
 
-function batch_perception(positive_data, negative_data)
-    positive = normalize(positive_data)
-    negative = normalize(negative_data) * -1
+function normalize_data(positive, negative)
+    " normalization adds additional one column to data "
+    normalize = data -> [data ones(size(data)[1])]
+
+    positive = normalize(positive)
+    negative = normalize(negative) * -1
     assert(size(positive) == size(negative))
     data = transpose([positive; negative])
 
     d, n = size(data)
     weight = zeros(d)
 
-    function compute_error(data, weight)
-        error_sum, error_count = zeros(d), 0
-        for i = 1:n
-            if dot(weight, data[:, i]) <= 0
-                error_sum += data[:, i]
-                error_count += 1
-            end
-        end
-        println("batch perception: weight $weight => $error_sum, $error_count")
-        error_sum, error_count
+    data, weight
+end
+
+function relax_procedure(positive, negative; eta=0.5, margin=0)
+    data, weight = normalize_data(positive, negative)
+    d, n = size(data)
+
+    println("data = $data")
+
+    function compute_error(data, weight, eta, margin)
+        error_samples = [y for y in filter([data[:, i] for i = 1:n]) do y
+            dot(y, weight) <= margin
+        end]
     end
 
-    error_sum, error_count = compute_error(data, weight)
-    step = 1
+    step, error_samples = 1, compute_error(data, weight, eta, margin)
+    while length(error_samples) > 0 #&& step <= 5
+        println("===============================================")
+        println("error_samples = $error_samples")
+        println("weight = $weight")
 
-    while error_count > 0
-        weight += error_sum
+        gradient = sum(error_samples) do y
+            println(y);
+            (margin - dot(weight, y) / dot(y, y)) * y
+        end
+
+        weight += eta * gradient
+        println("new weight $weight\t<= $gradient")
+
         step += 1
+        error_samples = compute_error(data, weight, eta, margin)
+    end
 
-        error_sum, error_count = compute_error(data, weight)
+    weight, step
+end
+
+function batch_perception(positive, negative)
+    data, weight = normalize_data(positive, negative)
+    d, n = size(data)
+
+    function compute_error(data, weight)
+        error_samples = [y for y in filter([data[:, i] for i = 1:n]) do y
+            dot(weight, y) <= 0
+        end]
+    end
+
+    step, error_samples = 1, compute_error(data, weight)
+
+    while length(error_samples) > 0
+        gradient = sum(error_samples)
+        weight += gradient
+        println("new weight $weight\t<= $gradient")
+
+        step += 1
+        error_samples = compute_error(data, weight)
     end
 
     weight, step
 end
 
 """
-Write a program to implement the “batch perception” algorithm (see page 44 or 45 in PPT).
-(a). Starting with a = 0, apply your program to the training data from 1 and 2. Note that the number of iterations required for convergence.
-(b). Apply your program to the training data from 3 and 2. Again, note that the number of iterations required for convergence.
+Write a program to implement the “batch perception” algorithm
+(see page 44 or 45 in PPT).
+
+(a). Starting with a = 0, apply your program to the training data from 1 and 2.
+Note that the number of iterations required for convergence.
+
+(b). Apply your program to the training data from 3 and 2.
+Again, note that the number of iterations required for convergence.
+
 (c). Explain the difference between the iterations required in the above two cases.
 """
 function hw2_1()
@@ -59,6 +101,7 @@ function hw2_1()
 end
 
 if !isinteractive()
-    hw2_1()
+    #hw2_1()
+    println(relax_procedure(SAMPLE[:, 1:2], SAMPLE[:, 3:4]))
 end
 
